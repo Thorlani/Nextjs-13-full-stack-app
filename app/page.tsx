8f3a766c74f06a29bd7659f4fdb0084a59e48303
Thorlani <tolanipopoola07@gmail.com>
2023-07-27 22:31:35 +0100

    added a postinstall command to the package.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-interface Post {
-  id: string;
-  title: string;
-  description: string;
-  date: string;
-}
-
 export default async function Home() {
-  const getPosts = async () => {
-    const res = await fetch("http://localhost:3000/api/posts");
-    const result = await res.json();
-    return result;
-  };
-  const { posts } = await getPosts();
   return (
-    <main className="w-full flex min-h-screen flex-col items-center justify-between p-24">
-      {posts.map((post: Post) => {
-        return (
-          <div key={post.id}>
-            <Link href={`/blog/post/${post.id}`}>
-              <h1 className="text-3xl font-bold">{post.title}</h1>
-              <p className="text-gray-600">{post.description}</p>
-              <p className="text-gray-600">{post.date.slice(0, 10)}</p>
-            </Link>
-          </div>
-        );
-      })}
+    <main className="w-full flex min-h-screen flex-col items-center justify-between p-20">
+      <Button variant={"link"}>
+        <Link href="/post">Get Started</Link>
+      </Button>
     </main>
   );
 }
---
+++ app/post/page.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+
+type Post = {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+};
+
+const Page = async () => {
+  const getPosts = async () => {
+    const res = await fetch("http://localhost:3000/api/posts", {
+      next: { revalidate: 60 },
+    });
+    const result = await res.json();
+    return result;
+  };
+  const { posts } = await getPosts();
+  return (
+    <main className="w-full flex min-h-screen flex-col items-start p-10">
+      {posts.map((post: Post) => {
+        return (
+          <div key={post.id} className="mb-8">
+            <Link href={`/post/${post.id}`}>
+              <h1 className="text-3xl font-bold">{post.title}</h1>
+              <p className="text-gray-600">{post.description}</p>
+              <p className="text-gray-600">{post.date.slice(0, 10)}</p>
+            </Link>
+          </div>
+        );
+      })}
+    </main>
+  );
+};
+
+export default Page;
