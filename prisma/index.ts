0b8c88f8d65dc9858d413f263d614ab36a267f2c
Thorlani <tolanipopoola07@gmail.com>
2023-07-27 23:01:32 +0100

    wip

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,24 +1,34 @@
+// import { PrismaClient } from "@prisma/client";
+
+// let prisma: PrismaClient;
+// declare global {
+//   namespace NodeJS {
+//     interface Global {
+//       prisma: PrismaClient;
+//     }
+//   }
+// }
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   //@ts-ignore
+//   if (!global.prisma) {
+//     //@ts-ignore
+//     global.prisma = new PrismaClient();
+//   }
+//   //@ts-ignore
+//   prisma = global.prisma;
+// }
+
+// export default prisma;
+
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
-declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma: PrismaClient;
-    }
-  }
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  //@ts-ignore
-  if (!global.prisma) {
-    //@ts-ignore
-    global.prisma = new PrismaClient();
-  }
-  //@ts-ignore
-  prisma = global.prisma;
-}
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-export default prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
