7e20a80b01e8dfcdd8b6bed88db61c387a278782
Thorlani <tolanipopoola07@gmail.com>
2023-10-10 10:57:22 +0100

    added state management

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,57 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/store/store";
+import { logOut, logIn } from "@/store/slice";
 
 const Navbar = () => {
+  const isCurrentUserAdmin = useSelector(
+    (state: RootState) => state.counter.value
+  );
+  const dispatch = useDispatch();
+  const [openModel, setOpenModel] = useState(false);
+
+  console.log(isCurrentUserAdmin);
+  const handleSubmit = () => {
+    dispatch(logIn());
+    setOpenModel(false);
+  };
   return (
-    <div className="flex justify-between items-center py-5 px-10">
-      <Link href={"/"}>
-        <h1 className="font-bold text-2xl italic">Thorlani</h1>
-      </Link>
-      <Link href={"/edit"}>
-        <p className="cursor-pointer">Edit Contents</p>
-      </Link>
-    </div>
+    <>
+      <div className="flex justify-between items-center py-5 px-10">
+        <Link href={"/"}>
+          <h1 className="font-bold text-2xl italic">Thorlani</h1>
+        </Link>
+        {isCurrentUserAdmin ? (
+          <Link href={"/edit"}>
+            <p className="cursor-pointer">Edit Contents</p>
+          </Link>
+        ) : (
+          <button onClick={() => setOpenModel(true)}>
+            <p className="cursor-pointer">Log In to Edit Content</p>
+          </button>
+        )}
+      </div>
+      {openModel && (
+        <div className="min-h-[100vh] min-w-[100vw] fixed top-0 left-0 z-[9999999] bg-black/90 flex justify-center items-center">
+          <div className="flex flex-col gap-4">
+            <input
+              className="w-[300px] h-[41px] indent-2 outline-none border-1px border-[#000]"
+              type="text"
+              placeholder="Admin's Name"
+            />
+            <button
+              className="w-fit h-fit py-3 px-10 bg-[gray] text-[#fff] rounded-[10px]"
+              type="button"
+              onClick={handleSubmit}
+            >
+              Enter
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- store/slice.ts
+++ store/slice.ts
@@ -1,35 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
 export interface CounterState {
-  value: number
+  value: boolean;
 }
 
 const initialState: CounterState = {
-  value: 0,
-}
+  value: false,
+};
 
 export const counterSlice = createSlice({
-  name: 'counter',
+  name: "counter",
   initialState,
   reducers: {
-    increment: (state) => {
+    logIn: (state) => {
       // Redux Toolkit allows us to write "mutating" logic in reducers. It
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.value += 1
+      state.value = true;
     },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload
+    logOut: (state) => {
+      state.value = false;
     },
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { logIn, logOut } = counterSlice.actions;
 
-export default counterSlice.reducer 
+export default counterSlice.reducer;
